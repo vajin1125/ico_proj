538a251e3eeb9becdd9454d1fb0f062a5abfea56
Alexandr <taka.alex99@hotmail.com>
2018-03-04 08:17:47 +0300

    Rich Text Editor with froala

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,5 @@ from .models import *
 # Register your models here.
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at','updated_at')
+    fields = ('title','photo','movie_url','sponsor','content')
 admin.site.register(News,NewsAdmin)

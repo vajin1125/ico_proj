e4eb58a19aed1fb3550c0b0620ad165339393e3b
Alexandr <taka.alex99@hotmail.com>
2018-03-13 17:35:23 +0300

    modify bloglist

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import *
 # Create your views here.
-def index(request,id = 0):
+def index(request):
+	news = News.objects.all()
+	return render(request,'blog/bloglist.html',{'news':news})
+def detail_blog(request,tt,id=0):
 	news = News.objects.all()
 	size = len(news)
 	id = int(id)
 	if(news):
 		news = news[id]
-	return render(request,'blog/bloglist.html',{'news':news,'id':id,'size':size-1})
+	return render(request,'blog/news.html',{'news':news,'id':id,'size':size-1})

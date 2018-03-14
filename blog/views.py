834426b87c9248c3975c01e384987196b404978e
vajin1125 <va.jin1125@hotmail.com>
2018-03-13 18:15:10 -0700

    modify in blogs

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import *
 # Create your views here.
-def index(request,id = 1):
+def index(request,id = 0):
 	news = News.objects.all()
 	size = len(news)
 	id = int(id)
 	if(news):
-		news = news[id-1]
-	return render(request,'blog/news.html',{'news':news,'size':size})
+		news = news[id]
+	return render(request,'blog/news.html',{'news':news,'id':id,'size':size-1})
